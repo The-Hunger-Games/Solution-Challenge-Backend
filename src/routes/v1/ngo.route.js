463fa17a4e69dae371887fd0c7f25ec49ef08421
soujanya hazra <soujanya0105@gmail.com>
2023-03-19 03:30:39 +0530

    ngo route added

---
+++ src/routes/v1/ngo.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const auth = require('../../middlewares/auth');
+const validate = require('../../middlewares/validate');
+const NGOValidation = require('../../validations/ngo.validation');
+const NGOController = require('../../controllers/ngo.controller');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .post(auth('manageNGOs'), validate(NGOValidation.createNgo), NGOController.createNgo)
+  .get(auth('getNGOs'), validate(NGOValidation.getNgos), NGOController.getNgos);
+
+router
+  .route('/:userId')
+  .get(auth('getNGOs'), validate(NGOValidation.getNgo), NGOController.getNgo)
+  .patch(auth('manageNGOs'), validate(NGOValidation.updateNgo), NGOController.updateNgo)
+  .delete(auth('manageNGOs'), validate(NGOValidation.deleteNgo), NGOController.deleteNgo);
+
+module.exports = router;
